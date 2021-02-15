9cff739e0d01060e86d026bac180f12620f97cdb
artem <artemii.lulevich@gmail.com>
2021-02-15 20:25:31 +0200

    Added update methods for deposit and withdraw.

--- Part_11_Using_dataBase/RollbackSQL/rollback.py
+++ Part_11_Using_dataBase/RollbackSQL/rollback.py
@@ -1,20 +1,55 @@
+import sqlite3
+import pytz
+import datetime
+
+db = sqlite3.connect("accounts.sqlite")
+db.execute("CREATE TABLE IF NOT EXISTS "
+           "accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL)")
+db.execute("CREATE TABLE IF NOT EXISTS transactions (time TIMESTAMP NOT NULL,"
+           "account TEXT NOT NULL,"
+           "amount INTEGER NOT NULL,"
+           "PRIMARY KEY (time, account))")
+
+
 class Account:
 
+    @staticmethod
+    def _current_time():
+        return pytz.utc.localize(datetime.datetime.utcnow())
+
     def __init__(self, name: str, opening_balance: int = 0):
-        self.name = name
-        self._balance = opening_balance
-        print("Account created for {}".format(self.name), end='')
+        cursor = db.execute("SELECT name, balance FROM accounts WHERE (name = ?)", (name, ))
+        row = cursor.fetchone()
+
+        if row:
+            self.name, self._balance = row
+            print("Retrieved record for {}. ".format(self.name), end='')
+        else:
+            self.name = name
+            self._balance = opening_balance
+            cursor.execute("INSERT INTO accounts VALUES(?, ?)", (name, opening_balance))
+            cursor.connection.commit()
+            print("Account created for {} ".format(self.name), end='')
+
         self.show_balance()
 
+    def _save_update(self, amount):
+        new_balance = self._balance + amount
+        time = Account._current_time()
+        db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))
+        db.execute("INSERT INTO transactions VALUES(?, ?, ?)", (time, self.name, amount))
+        db.commit()
+        self._balance = new_balance
+
     def deposit(self, amount: int) -> float:
-        if amount > 0:
-            self._balance += amount
+        if amount > 0.0:
+            self._save_update(amount)
             print("{:.2f} deposited".format(amount / 100))
         return self._balance / 100
 
     def withdraw(self, amount: int) -> float:
         if 0 < amount <= self._balance:
-            self._balance -= amount
+            self._save_update(-amount)
             print("{:.2f} withdrawn". format(amount / 100))
             return amount / 100
         else:
@@ -33,3 +68,9 @@ if __name__ == '__main__':
     john.withdraw(30)
     john.withdraw(0)
     john.show_balance()
+
+    terry = Account('Terry')
+    graham = Account("Graham", 9000)
+    eric = Account("Eric", 7000)
+
+    db.close()
